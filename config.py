6faa7334cfc2f3b4e8c63a8e922c5b1a146613c0
HexShield <elio1264ahmar@gmail.com>
2025-04-13 14:35:34 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+# config/config.py
+
+tkn = "MTMzMTI1NzM5NjE1NjQzMjM5NA.G5C70y.7Y8CynsL5THgA7MCZzYH37nqKH1rSr3tnrjX2M"
+secret = "0SBDZkL8qoP1uv19nxdQvKygTzFOHm7Y"
+client_id = "1359973597946576947"
+redirect = "http://localhost:8080"
